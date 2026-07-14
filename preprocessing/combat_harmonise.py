60dc61d11f72a552de191df3cb0b710b7c4554b1
AliAfsharmoqaddam <155386944+AliAfsharmoqaddam@users.noreply.github.com>
2026-07-14 13:44:35 +0100

    clean up & new hdf5

--- preprocessing/combat_harmonise.py
+++ preprocessing/combat_harmonise.py
@@ -4,34 +4,28 @@ from meld_classifier.paths import BASE_PATH, COMBAT_PARAMS_FILE
 from meld_classifier.meld_cohort import MeldCohort
 from meld_classifier.data_preprocessing import Preprocess
 
-#Define paths
-DATASET_FILE = '/home/meldstudent/Documents/RDS_NeoHipp/final_dataset_no_fcd.csv'
+#Define combat file path
 combat_file = os.path.join(BASE_PATH, COMBAT_PARAMS_FILE)
 
 #Generate feature names
-feature_list = []
+features = []
 
 for d in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-    feature_list.append(f".on_lh.gm_T1_{d}.sm5.mgh")
+    features.append(f".on_lh.gm_T1_{d}.sm5.mgh")
 
 for d in np.linspace(0,-7,29):
-    feature_list.append(f".on_lh.gm_T1_{d}mm.sm5.mgh")
+    features.append(f".on_lh.gm_T1_{d}mm.sm5.mgh")
 
 for d in [0, -0.5, -1, -1.5, -2, -2.5, -3, -3.5, -4]:
-    feature_list.append(f".on_lh.wm_T1_{d}mm.sm5.mgh")
+    features.append(f".on_lh.wm_T1_{d}mm.sm5.mgh")
 
-#Main function for ComBat  
+#Main function for ComBat with corresponding .hdf5  
 def main():
-    c_smooth = MeldCohort(hdf5_file_root="{site_code}_{group}_featurematrix_smoothed_new_3.hdf5", dataset=DATASET_FILE)
-    combat = Preprocess(
-        c_smooth,
-        write_hdf5_file_root="{site_code}_{group}_featurematrix_combat_new_no_fcd.hdf5",
-        data_dir=BASE_PATH,
-    )
-    for feature in feature_list:
+    c_smooth = MeldCohort(hdf5_file_root="{site_code}_{group}_featurematrix_smoothed_h16_new.hdf5")
+    combat = Preprocess(c_smooth, write_hdf5_file_root="{site_code}_{group}_featurematrix_combat_h16_new.hdf5", data_dir=BASE_PATH)
+    for feature in features:
         print(feature)
         combat.combat_whole_cohort(feature, outliers_file=None, combat_params_file=combat_file)
 
 if __name__ == "__main__":
     main()
-
